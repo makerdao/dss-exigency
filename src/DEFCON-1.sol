ec3bbf23495159729181c61768fd1257e616f1e8
Christopher Mooney <chris@dod.net>
2020-07-02 13:15:20 -0400

    updated DEFCON spells for T2020_10_01_1200UTC

--- src/DEFCON-1.sol
+++ src/DEFCON-1.sol
@@ -15,12 +15,48 @@
 
 pragma solidity 0.5.12;
 
-import "lib/dss-interfaces/src/dapp/DSPauseAbstract.sol";
-import "lib/dss-interfaces/src/dss/PotAbstract.sol";
-import "lib/dss-interfaces/src/dss/JugAbstract.sol";
-import "lib/dss-interfaces/src/dss/VatAbstract.sol";
-import "lib/dss-interfaces/src/dss/FlipAbstract.sol";
-import "lib/dss-interfaces/src/dss/FlipperMomAbstract.sol";
+// https://github.com/dapphub/ds-pause
+contract DSPauseAbstract {
+    function delay() public view returns (uint256);
+    function plot(address, bytes32, bytes memory, uint256) public;
+    function exec(address, bytes32, bytes memory, uint256) public returns (bytes memory);
+}
+
+// https://github.com/makerdao/dss/blob/master/src/pot.sol
+contract PotAbstract {
+    function file(bytes32, uint256) external;
+    function drip() external returns (uint256);
+}
+
+// https://github.com/makerdao/dss/blob/master/src/jug.sol
+contract JugAbstract {
+    function file(bytes32, bytes32, uint256) external;
+    function drip(bytes32) external returns (uint256);
+}
+
+// https://github.com/makerdao/dss/blob/master/src/vat.sol
+contract VatAbstract {
+    function ilks(bytes32) external view returns (uint256, uint256, uint256, uint256, uint256);
+    function file(bytes32, uint256) external;
+    function file(bytes32, bytes32, uint256) external;
+}
+
+// https://github.com/makerdao/dss/blob/master/src/flip.sol
+contract FlipAbstract {
+    function file(bytes32, uint256) external;
+}
+
+// https://github.com/makerdao/flipper-mom/blob/master/src/FlipperMom.sol
+contract FlipperMomAbstract {
+    function rely(address) external;
+    function deny(address) external;
+}
+
+// https://github.com/makerdao/ilk-registry/blob/master/src/IlkRegistry.sol
+contract IlkRegistryAbstract {
+    function list() external view returns (bytes32[] memory);
+    function flip(bytes32 ilk) external view returns (address);
+}
 
 contract SpellAction {
     // Provides a descriptive tag for bot consumption
@@ -39,16 +75,10 @@ contract SpellAction {
         0x19c0976f590D67707E62397C87829d896Dc0f1F1;
     address constant public MCD_POT =
         0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
-    address constant public MCD_FLIP_ETH_A =
-        0xd8a04F5412223F513DC55F839574430f5EC15531;
-    address constant public MCD_FLIP_BAT_A =
-        0xaA745404d55f88C108A28c86abE7b5A1E7817c07;
-    address constant public MCD_FLIP_WBTC_A =
-        0x3E115d85D4d7253b05fEc9C0bB5b08383C2b0603;
-    address constant public MCD_FLIP_USDC_A =
-        0xE6ed1d09a19Bd335f051d78D5d22dF3bfF2c28B1;
     address constant public FLIPPER_MOM =
         0x9BdDB99625A711bf9bda237044924E34E8570f75;
+    address constant public ILK_REGISTRY =
+        0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef;
 
 
     // Many of the settings that change weekly rely on the rate accumulator
@@ -64,16 +94,13 @@ contract SpellAction {
     uint256 constant public MILLION = 10**6;
 
     function execute() external {
-
-        // Drip Pot and Jugs prior to all modifications.
-        PotAbstract(MCD_POT).drip();
-        JugAbstract(MCD_JUG).drip("ETH-A");
-        JugAbstract(MCD_JUG).drip("BAT-A");
-        JugAbstract(MCD_JUG).drip("USDC-A");
-        JugAbstract(MCD_JUG).drip("WBTC-A");
-
+        uint256 totalLine = 0;
 
         // MCD Modifications
+
+        // Ensure we drip pot prior to modifications (housekeeping).
+        //
+        PotAbstract(MCD_POT).drip();
 
         // Set the Dai Savings Rate
         // DSR_RATE is a value determined by the rate accumulator calculation
@@ -82,109 +109,78 @@ contract SpellAction {
         uint256 DSR_RATE = ZERO_PCT_RATE;
         PotAbstract(MCD_POT).file("dsr", DSR_RATE);
 
-        // Set the ETH-A stability fee
-        // ETH_A_FEE is a value determined by the rate accumulator calculation.
-        // ex. an 8% annual rate will be 1000000002440418608258400030
+        // Loop over all ilks
         //
-        uint256 ETH_A_FEE = ZERO_PCT_RATE;
-        JugAbstract(MCD_JUG).file("ETH-A", "duty", ETH_A_FEE);
+        IlkRegistryAbstract registry = IlkRegistryAbstract(ILK_REGISTRY);
+        bytes32[] memory ilks = registry.list();
 
-        // Set the BAT-A stability fee
-        // BAT_A_FEE is a value determined by the rate accumulator calculation.
-        // ex. an 8% annual rate will be 1000000002440418608258400030
+        for (uint i = 0; i < ilks.length; i++) {
+            // Always drip the ilk prior to modifications (housekeeping)
+            //
+            JugAbstract(MCD_JUG).drip(ilks[i]);
+
+            // Set the ilk's flip tau
+            //
+            FlipAbstract(registry.flip(ilks[i])).file(bytes32("tau"), 24 hours);
+
+            // skip the rest of the loop for the following ilks:
+            //
+            if (ilks[i] == "USDC-B") {
+                continue;
+            }
+
+            // Set the ilk stability fee
+            //
+            JugAbstract(MCD_JUG).file(ilks[i], "duty", ZERO_PCT_RATE);
+
+            // Keep a running total of all ilk Debt Ceilings
+            //
+            (,,, uint256 ilkLine,) = VatAbstract(MCD_VAT).ilks(ilks[i]);
+            totalLine += ilkLine;
+
+            // skip the rest of the loop for the following ilks:
+            //
+            if (ilks[i] == "USDC-A" || ilks[i] == "TUSD-A") {
+                continue;
+            }
+
+            // Enable collateral liquidations
+            //
+            // This change will enable liquidations for collateral types
+            // and is colloquially referred to as the "circuit breaker".
+            //
+            // NOTE: We flipped the liquidation breaker off in the schedule(),
+            // waited the GSM delay period, changed system parameters, and are
+            // now re-enabling liquidations.  We are also not re-enabling some
+            // liquidations as they were never enabled.
+            //
+            FlipperMomAbstract(FLIPPER_MOM).rely(registry.flip(ilks[i]));
+        }
+
+        // Set the USDC-B stability fee
         //
-        uint256 BAT_A_FEE = ZERO_PCT_RATE;
-        JugAbstract(MCD_JUG).file("BAT-A", "duty", BAT_A_FEE);
+        JugAbstract(MCD_JUG).file("USDC-B", "duty", FIFTY_PCT_RATE);
 
-        // Set the WBTC-A stability fee
-        // WBTC_A_FEE is a value determined by the rate accumulator calculation.
-        // ex. an 8% annual rate will be 1000000002440418608258400030
-        //
-        uint256 WBTC_A_FEE = ZERO_PCT_RATE;
-        JugAbstract(MCD_JUG).file("WBTC-A", "duty", WBTC_A_FEE);
-
-        // Set the USDC stability fee
-        // USDC_FEE is a value determined by the rate accumulator calculation.
-        // ex. an 8% annual rate will be 1000000002440418608258400030
-        //
-        uint256 USDC_A_FEE = FIFTY_PCT_RATE;
-        JugAbstract(MCD_JUG).file("USDC-A", "duty", USDC_A_FEE);
-
-        // Set the USDC-A debt ceiling
-        // USDC_LINE is the number of Dai that can be created with USDC token
+        // Set the USDC-B debt ceiling
+        // USDC_B_LINE is the number of Dai that can be created with USDC token
         // collateral.
-        // ex. a 40 million Dai USDC ceiling will be USDC_LINE = 40000000
+        // ex. a 40 million Dai USDC-B ceiling will be USDC_B_LINE = 40000000
         //
-        // Existing Line: 20m
+        // Existing Line: 10m
         // New Line: 40m
-        uint256 USDC_A_LINE = 40 * MILLION;
-        VatAbstract(MCD_VAT).file("USDC-A", "line", USDC_A_LINE * RAD);
+        uint256 USDC_B_LINE = 40 * MILLION * RAD;
+        VatAbstract(MCD_VAT).file("USDC-B", "line", USDC_B_LINE);
+        totalLine += USDC_B_LINE;
 
-        // Set the global debt ceiling
+        // Set the Global Debt Ceiling to the sum of all ilk line
         //
-        // GLOBAL_LINE is the total number of Dai that can be created by all
-        // collateral types as a whole number
-        //
-        (,,, uint256 saiLine,)   = VatAbstract(MCD_VAT).ilks("SAI");
-        (,,, uint256 ethALine,)  = VatAbstract(MCD_VAT).ilks("ETH-A");
-        (,,, uint256 batALine,)  = VatAbstract(MCD_VAT).ilks("BAT-A");
-        (,,, uint256 wbtcALine,) = VatAbstract(MCD_VAT).ilks("WBTC-A");
-        (,,, uint256 usdcALine,) = VatAbstract(MCD_VAT).ilks("USDC-A");
-        uint256 GLOBAL_LINE =
-            saiLine + ethALine + batALine + wbtcALine + usdcALine;
-        VatAbstract(MCD_VAT).file("Line", GLOBAL_LINE);
-
-        // Set the ETH-A Flip tau
-        //
-        // ETH_A_FLIP_TAU is the bid lifetime
-        //
-        uint256 ETH_A_FLIP_TAU = 24 hours;
-        FlipAbstract(MCD_FLIP_ETH_A).file(bytes32("tau"), ETH_A_FLIP_TAU);
-
-        // Set the BAT-A Flip tau
-        //
-        // BAT_A_FLIP_TAU is the bid lifetime
-        //
-        uint256 BAT_A_FLIP_TAU = 24 hours;
-        FlipAbstract(MCD_FLIP_BAT_A).file(bytes32("tau"), BAT_A_FLIP_TAU);
-
-        // Set the WBTC-A Flip tau
-        //
-        // WBTC_A_FLIP_TAU is the bid lifetime
-        //
-        uint256 WBTC_A_FLIP_TAU = 24 hours;
-        FlipAbstract(MCD_FLIP_WBTC_A).file(bytes32("tau"), WBTC_A_FLIP_TAU);
-
-        // Set the USDC-A Flip tau
-        //
-        // USDC_A_FLIP_TAU is the bid lifetime
-        //
-        uint256 USDC_A_FLIP_TAU = 24 hours;
-        FlipAbstract(MCD_FLIP_USDC_A).file(bytes32("tau"), USDC_A_FLIP_TAU);
-
-        // Enable all collateral liquidations
-        //
-        // This change will enable liquidations across all collateral types
-        // and is colloquially referred to as the circuit breaker.
-        //
-        // NOTE: We flipped the liquidation breaker off in the schedule(),
-        // waited the GSM delay period, changed system parameters, and are now
-        // re-enabling liquidations.  We are also not re-enabling USDC-A
-        // liquidations as they were never enabled, the OSM price is locked to
-        // $1, and we don't have a healthy keeper ecosystem for USDC-A yet.
-        //
-        FlipperMomAbstract(FLIPPER_MOM).rely(MCD_FLIP_ETH_A);
-        FlipperMomAbstract(FLIPPER_MOM).rely(MCD_FLIP_BAT_A);
-        FlipperMomAbstract(FLIPPER_MOM).rely(MCD_FLIP_WBTC_A);
-        // FlipperMomAbstract(FLIPPER_MOM).rely(MCD_FLIP_USDC_A);
-
+        VatAbstract(MCD_VAT).file("Line", totalLine);
     }
 }
 
 contract DssSpell {
 
-    DSPauseAbstract  public pause =
-        DSPauseAbstract(0xbE286431454714F511008713973d3B053A2d38f3);
+    DSPauseAbstract  public pause;
     address          public action;
     bytes32          public tag;
     uint256          public eta;
@@ -192,19 +188,15 @@ contract DssSpell {
     uint256          public expiration;
     bool             public done;
 
-    address constant public MCD_FLIP_ETH_A =
-        0xd8a04F5412223F513DC55F839574430f5EC15531;
-    address constant public MCD_FLIP_BAT_A =
-        0xaA745404d55f88C108A28c86abE7b5A1E7817c07;
-    address constant public MCD_FLIP_USDC_A =
-        0xE6ed1d09a19Bd335f051d78D5d22dF3bfF2c28B1;
-    address constant public MCD_FLIP_WBTC_A =
-        0x3E115d85D4d7253b05fEc9C0bB5b08383C2b0603;
+    address constant public MCD_PAUSE =
+        0xbE286431454714F511008713973d3B053A2d38f3;
     address constant public FLIPPER_MOM =
         0x9BdDB99625A711bf9bda237044924E34E8570f75;
+    address constant public ILK_REGISTRY =
+        0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef;
 
     uint256 constant internal MILLION = 10**6;
-    uint256 constant public T2020_07_01_1200UTC = 1593604800;
+    uint256 constant public T2020_10_01_1200UTC = 1601553600;
 
     constructor() public {
         sig = abi.encodeWithSignature("execute()");
@@ -213,7 +205,8 @@ contract DssSpell {
         address _action = action;
         assembly { _tag := extcodehash(_action) }
         tag = _tag;
-        expiration = T2020_07_01_1200UTC;
+        pause = DSPauseAbstract(MCD_PAUSE);
+        expiration = T2020_10_01_1200UTC;
     }
 
     function description() public view returns (string memory) {
@@ -223,21 +216,22 @@ contract DssSpell {
     function schedule() public {
         require(now <= expiration, "This contract has expired");
         require(eta == 0, "This spell has already been scheduled");
-        eta = now + DSPauseAbstract(pause).delay();
+        eta = now + pause.delay();
         pause.plot(action, tag, sig, eta);
 
-        // NOTE: 'eta' check should mimic the old behavior of 'done', thus
-        // preventing these changes from being executed again.
+        // Loop over all ilks
+        //
+        IlkRegistryAbstract registry = IlkRegistryAbstract(ILK_REGISTRY);
+        bytes32[] memory ilks = registry.list();
 
-        // Disable all collateral liquidations
-        //
-        // This change will prevent liquidations across all collateral types
-        // and is colloquially referred to as the circuit breaker.
-        //
-        FlipperMomAbstract(FLIPPER_MOM).deny(MCD_FLIP_ETH_A);
-        FlipperMomAbstract(FLIPPER_MOM).deny(MCD_FLIP_BAT_A);
-        FlipperMomAbstract(FLIPPER_MOM).deny(MCD_FLIP_WBTC_A);
-        FlipperMomAbstract(FLIPPER_MOM).deny(MCD_FLIP_USDC_A);
+        for (uint i = 0; i < ilks.length; i++) {
+            // Disable all collateral liquidations
+            //
+            // This change will prevent liquidations across all collateral types
+            // and is colloquially referred to as the circuit breaker.
+            //
+            FlipperMomAbstract(FLIPPER_MOM).deny(registry.flip(ilks[i]));
+        }
     }
 
     function cast() public {
