907f4885d201a9ea6ac69eb723cbf9cbca0b5b7a
Emilio Silva Schlenker <e18r@disroot.org>
2021-01-12 09:34:24 -0500

    DEFCON-1: also add the chainlog in DssSpell

--- src/DEFCON-1.sol
+++ src/DEFCON-1.sol
@@ -15,6 +15,8 @@
 
 pragma solidity 0.5.12;
 
+import "lib/dss-interfaces/src/dss/ChainlogAbstract.sol";
+
 // https://github.com/dapphub/ds-pause
 contract DSPauseAbstract {
     function delay() public view returns (uint256);
@@ -57,8 +59,6 @@ contract IlkRegistryAbstract {
     function list() external view returns (bytes32[] memory);
     function flip(bytes32) external view returns (address);
 }
-
-import "lib/dss-interfaces/src/dss/ChainlogAbstract.sol";
 
 contract SpellAction {
     // This address should correspond to the latest MCD Chainlog contract; verify
@@ -147,6 +147,11 @@ contract SpellAction {
 }
 
 contract DssSpell {
+    // This address should correspond to the latest MCD Chainlog contract; verify
+    //  against the current release list at:
+    //     https://changelog.makerdao.com/releases/mainnet/active/contracts.json
+    ChainlogAbstract constant CHANGELOG =
+        ChainlogAbstract(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
 
     DSPauseAbstract  public pause;
     address          public action;
@@ -156,9 +161,9 @@ contract DssSpell {
     uint256          public expiration;
     bool             public done;
 
-    address constant MCD_PAUSE    = 0xbE286431454714F511008713973d3B053A2d38f3;
-    address constant FLIPPER_MOM  = 0xc4bE7F74Ee3743bDEd8E0fA218ee5cf06397f472;
-    address constant ILK_REGISTRY = 0x8b4ce5DCbb01e0e1f0521cd8dCfb31B308E52c24;
+    address constant MCD_PAUSE    = CHANGELOG.getAddress("MCD_PAUSE");
+    address constant FLIPPER_MOM  = CHANGELOG.getAddress("FLIPPER_MOM");
+    address constant ILK_REGISTRY = CHANGELOG.getAddress("ILK_REGISTRY");
 
     uint256 constant T2021_02_01_1200UTC = 1612180800;
 
