8866a2432266224c1bc267ec03b06b3052f02486
Brian McMichael <brian@brianmcmichael.com>
2021-04-21 15:36:46 -0400

    Updates for latest dapp

--- test-dssspell.sh
+++ test-dssspell.sh
@@ -3,10 +3,11 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-SOLC_FLAGS="--optimize --optimize-runs=1" dapp --use solc:0.5.12 build
+export DAPP_BUILD_OPTIMIZE=1
+export DAPP_BUILD_OPTIMIZE_RUNS=1
 
-export DAPP_TEST_ADDRESS=0x8EE7D9235e01e6B42345120b5d270bdB763624C7
-export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
-export DAPP_TEST_NUMBER=$(seth block latest number)
-
-LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1 --match='testDEFCON[1-5]'
+if [[ -z "$1" ]]; then
+  dapp --use solc:0.5.12 test --rpc-url="$ETH_RPC_URL" --verbose 1 --match='testDEFCON[1-5]'
+else
+  dapp --use solc:0.5.12 test --rpc-url="$ETH_RPC_URL" --match "$1" -vv
+fi
